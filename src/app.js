02cc2b4bc0a3aab31aeb8ba744746d4adb791e34
saipatel96 <95169758+saipatel96@users.noreply.github.com>
2022-09-20 01:33:18 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,24 @@
 
+import "./App.css";
+
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
+import Tasks from "./components/Tasks";
+
+function App() {
+  return (
+    <div>
+      <section>
+        <div className="bar-container">
+          <Sidebar />
+          <div>
+            <Navbar />
+            <Tasks />
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default App;
