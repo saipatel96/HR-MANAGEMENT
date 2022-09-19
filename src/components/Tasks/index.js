f15c99f368da8279042644b0bf84545dcf5ea5f8
saipatel96 <95169758+saipatel96@users.noreply.github.com>
2022-09-20 01:43:01 +0530

    Update index.js

--- src/components/Tasks/index.js
+++ src/components/Tasks/index.js
@@ -1,1 +1,109 @@
 
+import React from "react";
+
+import TasksBottomContainer from "../TasksBottomContainer";
+
+import "./index.css";
+
+const Tasks = () => {
+  return (
+    <>
+      <div className="tasks-container">
+        <div className="task-align">
+          <div className="first-task">
+            <div className="container">
+              <h4 className="text"> Tasks </h4>
+              <div className="calender-container">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-chevron-left calendar-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"
+                  />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-calendar4-week calendar-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path d="M3.5 0a.5.5 0 0 1 .5.5V1h8V.5a.5.5 0 0 1 1 0V1h1a2 2 0 0 1 2 2v11a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V3a2 2 0 0 1 2-2h1V.5a.5.5 0 0 1 .5-.5zM2 2a1 1 0 0 0-1 1v1h14V3a1 1 0 0 0-1-1H2zm13 3H1v9a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1V5z" />
+                  <path d="M11 7.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-2 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1zm-3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1z" />
+                </svg>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="16"
+                  height="16"
+                  fill="currentColor"
+                  className="bi bi-chevron-right calendar-icon"
+                  viewBox="0 0 16 16"
+                >
+                  <path
+                    fill-rule="evenodd"
+                    d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708z"
+                  />
+                </svg>
+              </div>
+            </div>
+            <hr className="line" />
+            <div className="task-align">
+              <div className="task-bottom-container">
+                <div className="tasks task-1">
+                  <h5>
+                    66 <br /> Ongoing
+                  </h5>
+                </div>
+              </div>
+              <div className="task-bottom-container">
+                <div className="tasks task-2">
+                  <h5>
+                    66 <br /> Ongoing
+                  </h5>
+                </div>
+              </div>
+              <div className="task-bottom-container">
+                <div className="tasks task-3">
+                  <h5>
+                    66 <br /> Ongoing
+                  </h5>
+                </div>
+              </div>
+              <div className="task-bottom-container">
+                <div className="tasks task-4">
+                  <h5>
+                    66 <br /> Ongoing
+                  </h5>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <button className="add-button">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="16"
+              height="16"
+              fill="currentColor"
+              className="bi bi-plus plus-icon"
+              viewBox="0 0 16 16"
+            >
+              <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+            </svg>
+            Add New Task
+          </button>
+        </div>
+        <TasksBottomContainer />
+      </div>
+    </>
+  );
+};
+
+export default Tasks;
